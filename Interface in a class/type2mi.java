c727664a50762c7fde14dd2dc800593c328052a6
Arpit Sharma <arpitvishwa01@gmail.com>
2025-08-21 21:17:52 +0530

    DSA

---
+++ Interface in a class/type2mi.java
@@ -0,0 +1,16 @@
+//Write a java program to implement multiple inheritance using interfaces academic & Sports via Student class show both academic and sports activity
+
+public class type2mi {
+    public static void main(String name, int marks, String sports) {
+        name = "Arpit";
+        marks = 8;
+        sports = "Cricket";
+        System.out.println(name + " scored " + marks + " CGPA in Academics.");
+
+        name = "Ayush";
+        marks = 9;
+        sports = "Football";
+        System.out.println(name + " scored " + marks + " CGPA in Academics.");
+        
+    }
+}
